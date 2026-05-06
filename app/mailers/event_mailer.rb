640db0f5276becaa23b22669f9899a0544a886e7
Albert Chae <albertchae@users.noreply.github.com>
2026-05-06 05:24:21 -0700

    Rename ancestor_* to self_and_ancestor_*
    Because we include self.id in ancestor_ids, self gets included everywhere

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -93,7 +93,7 @@ class EventMailer < ApplicationMailer
     @whodunnit = params[:whodunnit]
 
     # Not using role_at_least? because we want this to be false for HCB admins
-    @is_manager = @event.ancestor_organizer_positions.map(&:user).include?(@whodunnit)
+    @is_manager = @event.self_and_ancestor_organizer_positions.map(&:user).include?(@whodunnit)
   end
 
 end
